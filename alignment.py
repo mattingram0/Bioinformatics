6711803098bd069bffd1a342e3a99f8583f54cb3
Matt Ingram <mattingram0@gmail.com>
2019-11-19 16:22:21 +0000

    Initial commit. File creation and function definitions.

---
+++ alignment.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sys
+
+def dynprog(alphabet, scoring_matrix, sequence1, sequence2):
+    pass
+
+
+def dynproglin(alphabet, scoring_matrix, sequence1, sequence2):
+    pass
+
+
+def dynproglin(alphabet, scoring_matrix, sequence1, sequence2):
+    pass
+
+
+def main():
+
+    # Parse Input
+    alphabet = sys.argv[1]
+    scoring_matrix = np.array((sys.argv[2]))
+    seq1 = sys.argv[3]
+    seq2 = sys.argv[4]
+
+main()
